f416656dbc2d8f6945d7197e2fba3a6a910bfa3d
Volodymyr Ivashchenko <110674732+ivashchenko-volodymyr@users.noreply.github.com>
2026-05-01 14:54:24 +0200

    Add docker commands to bash_commands.sh
    Added docker commands for managing images and containers.

--- bash_commands.sh
+++ bash_commands.sh
@@ -1,2 +1,9 @@
 # connan commands
 conan audit scan . --out-file="../audit_$(basename "$PWD")_$(date +%Y%m%d_%H%M%S_%3N).txt"
+
+# docker commands
+# to delete a docker image delete all related docker containers first
+sudo docker container ls -a
+sudo docker container rm 354446943b1a
+sudo docker iamge ls
+sudo docker image rm ef54e839ef54
