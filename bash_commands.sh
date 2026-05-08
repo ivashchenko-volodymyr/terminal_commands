39a99091bf4aec9431c8571a8697d9d0e9144aeb
Volodymyr Ivashchenko <110674732+ivashchenko-volodymyr@users.noreply.github.com>
2026-05-08 10:21:49 +0200

    bash command to delete local git branches which are merged to tge based branch, like develop or main

--- bash_commands.sh
+++ bash_commands.sh
@@ -7,3 +7,5 @@ sudo docker container ls -a
 sudo docker container rm 354446943b1a
 sudo docker iamge ls
 sudo docker image rm ef54e839ef54
+
+git fetch -p && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -d
