68d99934544bf827576d7f8467de40b4b94e9566
Volodymyr Ivashchenko <110674732+ivashchenko-volodymyr@users.noreply.github.com>
2026-05-22 14:48:05 +0200

    command to list installed applications on a Windows machine

--- batch_commands.bat
+++ batch_commands.bat
@@ -1,1 +1,5 @@
 pip freeze > %USERPROFILE%\pip_packages.txt && pip uninstall-r %USERPROFILE%\pip_packages.txt -y && del %USERPROFILE%\pip_packages.txt
+
+
+wmic product get name,version
+wmic /output:C:\InstalledPrograms.txt product get name,version
